c7898aa3bb336e156112728fdc9ae47e50f263ce
VeraIgnazio <miEmail@domain.com>
2024-07-02 09:14:46 -0300

    revision mostrar ocultar, ejercicio grados radianes terminado y respectivas funciones js actualizadas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -13,31 +13,31 @@ function CambiarUnidades(id, valor) {
         document.lasUnidades.unid_pie.value = "";
         document.lasUnidades.unid_yarda.value = "";
     } else if (id === "metro") {
-        document.lasUnidades.unid_pulgada.value = 39.3701 * valor;
-        document.lasUnidades.unid_pie.value = 3.28084 * valor;
-        document.lasUnidades.unid_yarda.value = 1.09361 * valor;
-    } else if (id === "pulgada") {
-        document.lasUnidades.unid_metro.value = 0.0245 * valor;
-        document.lasUnidades.unid_pie.value = 0.0833333 * valor;
-        document.lasUnidades.unid_yarda.value = 0.0277777 * valor;
-    } else if (id === "`pie`") {
-        document.lasUnidades.unid_metro.value = 39.3701 * valor;
-        document.lasUnidades.unid_pulgada.value = 3.28084 * valor;
-        document.lasUnidades.unid_yarda.value = 1.09361 * valor;
-    } else if (id === "yarda") {
-        document.lasUnidades.unid_pulgada.value = 0.9144 * valor;
-        document.lasUnidades.unid_pie.value = 36 * valor;
-        document.lasUnidades.unid_metro.value = 4 * valor;
+        document.lasUnidades.pulgada.value = valor * 39.3701;
+        document.lasUnidades.pie.value = valor * 3.28084;
+        document.lasUnidades.yarda.value = valor * 1.09361;
+    } else if (unidad === "pulgada") {
+        document.lasUnidades.metro.value = valor * 0.0254;
+        document.lasUnidades.pie.value = valor * 0.08333;
+        document.lasUnidades.yarda.value = valor * 0.027778;
+    } else if (unidad === "pie") {
+        document.lasUnidades.metro.value = valor * 0.3048;
+        document.lasUnidades.pulgada.value = valor * 12;
+        document.lasUnidades.yarda.value = valor * 0.333333;
+    } else if (unidad === "yarda") {
+        document.lasUnidades.metro.value = valor * 0.9144;
+        document.lasUnidades.pulgada.value = valor * 36;
+        document.lasUnidades.pie.value = valor * 3;
     }
 }
 {
-    function convertirGR(id) {
+    function ConvertirGR(id) {
         var grad, rad;
-        if (id=="grados"){
+        if (id== "grados"){
             grad = document.getElementById("grados").value;
-            rad=(grad*Math.PI)/180;
-        } else if (id==radianes){
-            rad=document.getElementById("radianes").value;
+            rad = (grad*Math.PI)/180;
+        } else if (id== "radianes"){
+            rad = document.getElementById("radianes").value;
             grad = (rad*180)/Math.PI;
         }
         document.getElementById("grados").value=grad;
@@ -58,7 +58,4 @@ function mostrarOcultar (valorMO){
     }
 
 }
-function dibujarCirculoCuadrado():void{
-    const canvas html element
 
-}
