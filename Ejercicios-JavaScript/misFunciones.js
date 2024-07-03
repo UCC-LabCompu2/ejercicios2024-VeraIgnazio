239481fe046c0c87cf022016f16043d9a5b59d6f
VeraIgnazio <miEmail@domain.com>
2024-07-02 23:45:30 -0300

    Ejercicio mini paint terminado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -144,3 +144,26 @@ function dibujarCirCuad(){
     ctx.fillStyle= "rgba(228,0,255,0.99)"
     ctx.fill();
 }
+var bandera;
+function dibujar(event){
+    var canvas = document.getElementById("CanvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function () {bandera = true};
+    canvas.onmouseup = function () {bandera = false};
+
+    if (bandera) {
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
+    }
+}
+function limpiarCanvas(){
+    var canvas = document.getElementById("CanvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
